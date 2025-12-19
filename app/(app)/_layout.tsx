943b6302822b41ad64863b12a73521d124e4bf0d
Kachielite <derrick.onyekachi@gmail.com>
2025-12-19 13:48:50 +0300

    feat: integrate zustand for user state management in layout

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import {Stack} from "expo-router";
+import React from 'react';
+import { Stack } from 'expo-router';
+import zustandStorage from '@/core/common/state';
 
 const _Layout = () => {
-    const user = false;
-    return (
-        <Stack>
-            <Stack.Protected guard={user}>
-                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            </Stack.Protected>
-            <Stack.Protected guard={!user}>
-                <Stack.Screen name="(public)" options={{ headerShown: false }} />
-            </Stack.Protected>
-        </Stack>
-    )
-}
-export default _Layout
+  const user = zustandStorage.getUser();
+  return (
+    <Stack>
+      <Stack.Protected guard={!!user}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack.Protected>
+      <Stack.Protected guard={!user}>
+        <Stack.Screen name="(public)" options={{ headerShown: false }} />
+      </Stack.Protected>
+    </Stack>
+  );
+};
+export default _Layout;
