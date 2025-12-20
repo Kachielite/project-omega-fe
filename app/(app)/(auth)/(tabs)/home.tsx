56f2b4ddc9314c6127d48f3cf99296b4c13f4836
Kachielite <derrick.onyekachi@gmail.com>
2025-12-20 09:01:10 +0300

    feat: add tab navigation layout with home, project, and settings screens

--- app/(app)/(auth)/(tabs)/home.tsx
+++ app/(app)/(auth)/(tabs)/home.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import useThemeColors from '@/core/common/hooks/use-theme-colors';
 
-export default function Index() {
+export default function Home() {
+  const colors = useThemeColors();
   return (
-    <View style={styles.container}>
-      <Text>Auth Tabs Index</Text>
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
+      <Text>Home</Text>
     </View>
   );
 }
