132b90c6311a7f850b355d9d9541658e2285cd2e
Kachielite <derrick.onyekachi@gmail.com>
2025-12-22 16:05:35 +0300

    feat: update task interfaces to support structured tags and enhance task creation/updating

--- features/task/interfaces/index.ts
+++ features/task/interfaces/index.ts
@@ -6,9 +6,14 @@ export interface ITask {
   status: ITaskStatus;
   due_date: string;
   priority: ITaskPriority;
-  tags: string[];
+  tags: ITags[];
   created_at: string;
   updated_at: string;
+}
+
+export interface ITags {
+  id: number;
+  name: string;
 }
 
 export enum ITaskStatus {
@@ -22,4 +27,35 @@ export enum ITaskPriority {
   LOW = 'Low',
   MEDIUM = 'Medium',
   HIGH = 'High',
+}
+
+export interface IQueryParams {
+  status?: ITaskStatus;
+  priority?: ITaskPriority;
+  due_date?: string;
+  tags?: string[];
+  search?: string;
+  sort_by?: 'due_date' | 'priority' | 'created_at' | 'title';
+  sort_order?: 'asc' | 'desc';
+  page: number;
+  limit: number;
+  project_id?: number;
+}
+
+export interface ICreateTaskDTO {
+  title: string;
+  description: string;
+  due_date: string;
+  priority: ITaskPriority;
+  tags?: number[];
+  project_id?: number;
+}
+
+export interface IUpdateTaskDTO {
+  title?: string;
+  description?: string;
+  status?: ITaskStatus;
+  due_date?: string;
+  priority?: ITaskPriority;
+  tags?: number[];
 }
